9376ac1fe013fc47796fee21298e3088f36ede72
Bjørn Sandvik <bjorn@mastermaps.com>
2023-08-13 14:30:35 +0200

    Layer update

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,10 @@
-console.log("validate.js");
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+const schema = require("../google-earth-engine/schema.json");
+const landcover = require("../google-earth-engine/landcover.json");
+
+const validate = ajv.compile(schema);
+
+const valid = validate(landcover);
+if (!valid) console.log(validate.errors);
