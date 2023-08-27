60973575a30c38fcb96532c98c9a13adcb8ae0bf
Bjørn Sandvik <bjorn@mastermaps.com>
2023-08-27 16:20:53 +0200

    Vector style

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,11 @@
 const Ajv = require("ajv");
 const ajv = new Ajv();
 
-const schema = require("../earthengine/schema.json");
-const landcover = require("../earthengine/precipitation.json");
+// const schema = require("../earthengine/schema.json");
+// const landcover = require("../earthengine/precipitation.json");
+
+const schema = require("../vectorstyle/schema.json");
+const landcover = require("../vectorstyle/voyager.json");
 
 const validate = ajv.compile(schema);
 
